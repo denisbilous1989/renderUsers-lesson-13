50d33151d5623adbfbbbbc9a33a2e66e87444986
denisbilous1989 <denisbilous1989@gmail.com>
2022-08-24 21:57:44 +0300

    renderUser function

---
+++ main.js
@@ -0,0 +1,428 @@
+let users = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  },
+  {
+    "id": 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "address": {
+      "street": "Douglas Extension",
+      "suite": "Suite 847",
+      "city": "McKenziehaven",
+      "zipcode": "59590-4157",
+      "geo": {
+        "lat": "-68.6102",
+        "lng": "-47.0653"
+      }
+    },
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info",
+    "company": {
+      "name": "Romaguera-Jacobson",
+      "catchPhrase": "Face to face bifurcated interface",
+      "bs": "e-enable strategic applications"
+    }
+  },
+  {
+    "id": 4,
+    "name": "Patricia Lebsack",
+    "username": "Karianne",
+    "email": "Julianne.OConner@kory.org",
+    "address": {
+      "street": "Hoeger Mall",
+      "suite": "Apt. 692",
+      "city": "South Elvis",
+      "zipcode": "53919-4257",
+      "geo": {
+        "lat": "29.4572",
+        "lng": "-164.2990"
+      }
+    },
+    "phone": "493-170-9623 x156",
+    "website": "kale.biz",
+    "company": {
+      "name": "Robel-Corkery",
+      "catchPhrase": "Multi-tiered zero tolerance productivity",
+      "bs": "transition cutting-edge web services"
+    }
+  },
+  {
+    "id": 5,
+    "name": "Chelsey Dietrich",
+    "username": "Kamren",
+    "email": "Lucio_Hettinger@annie.ca",
+    "address": {
+      "street": "Skiles Walks",
+      "suite": "Suite 351",
+      "city": "Roscoeview",
+      "zipcode": "33263",
+      "geo": {
+        "lat": "-31.8129",
+        "lng": "62.5342"
+      }
+    },
+    "phone": "(254)954-1289",
+    "website": "demarco.info",
+    "company": {
+      "name": "Keebler LLC",
+      "catchPhrase": "User-centric fault-tolerant solution",
+      "bs": "revolutionize end-to-end systems"
+    }
+  },
+  {
+    "id": 6,
+    "name": "Mrs. Dennis Schulist",
+    "username": "Leopoldo_Corkery",
+    "email": "Karley_Dach@jasper.info",
+    "address": {
+      "street": "Norberto Crossing",
+      "suite": "Apt. 950",
+      "city": "South Christy",
+      "zipcode": "23505-1337",
+      "geo": {
+        "lat": "-71.4197",
+        "lng": "71.7478"
+      }
+    },
+    "phone": "1-477-935-8478 x6430",
+    "website": "ola.org",
+    "company": {
+      "name": "Considine-Lockman",
+      "catchPhrase": "Synchronised bottom-line interface",
+      "bs": "e-enable innovative applications"
+    }
+  },
+  {
+    "id": 7,
+    "name": "Kurtis Weissnat",
+    "username": "Elwyn.Skiles",
+    "email": "Telly.Hoeger@billy.biz",
+    "address": {
+      "street": "Rex Trail",
+      "suite": "Suite 280",
+      "city": "Howemouth",
+      "zipcode": "58804-1099",
+      "geo": {
+        "lat": "24.8918",
+        "lng": "21.8984"
+      }
+    },
+    "phone": "210.067.6132",
+    "website": "elvis.io",
+    "company": {
+      "name": "Johns Group",
+      "catchPhrase": "Configurable multimedia task-force",
+      "bs": "generate enterprise e-tailers"
+    }
+  },
+  {
+    "id": 8,
+    "name": "Nicholas Runolfsdottir V",
+    "username": "Maxime_Nienow",
+    "email": "Sherwood@rosamond.me",
+    "address": {
+      "street": "Ellsworth Summit",
+      "suite": "Suite 729",
+      "city": "Aliyaview",
+      "zipcode": "45169",
+      "geo": {
+        "lat": "-14.3990",
+        "lng": "-120.7677"
+      }
+    },
+    "phone": "586.493.6943 x140",
+    "website": "jacynthe.com",
+    "company": {
+      "name": "Abernathy Group",
+      "catchPhrase": "Implemented secondary concept",
+      "bs": "e-enable extensible e-tailers"
+    }
+  },
+  {
+    "id": 9,
+    "name": "Glenna Reichert",
+    "username": "Delphine",
+    "email": "Chaim_McDermott@dana.io",
+    "address": {
+      "street": "Dayna Park",
+      "suite": "Suite 449",
+      "city": "Bartholomebury",
+      "zipcode": "76495-3109",
+      "geo": {
+        "lat": "24.6463",
+        "lng": "-168.8889"
+      }
+    },
+    "phone": "(775)976-6794 x41206",
+    "website": "conrad.com",
+    "company": {
+      "name": "Yost and Sons",
+      "catchPhrase": "Switchable contextually-based project",
+      "bs": "aggregate real-time technologies"
+    }
+  },
+  {
+    "id": 10,
+    "name": "Clementina DuBuque",
+    "username": "Moriah.Stanton",
+    "email": "Rey.Padberg@karina.biz",
+    "address": {
+      "street": "Kattie Turnpike",
+      "suite": "Suite 198",
+      "city": "Lebsackbury",
+      "zipcode": "31428-2261",
+      "geo": {
+        "lat": "-38.2386",
+        "lng": "57.2232"
+      }
+    },
+    "phone": "024-648-3804",
+    "website": "ambrose.net",
+    "company": {
+      "name": "Hoeger LLC",
+      "catchPhrase": "Centralized empowering task-force",
+      "bs": "target end-to-end models"
+    }
+  }
+];
+
+// {
+//   "id": 1,
+//   "name": "Leanne Graham",
+//   "username": "Bret",
+//   "email": "Sincere@april.biz",
+//   "address": {
+//     "street": "Kulas Light",
+//     "suite": "Apt. 556",
+//     "city": "Gwenborough",
+//     "zipcode": "92998-3874",
+//     "geo": {
+//       "lat": "-37.3159",
+//       "lng": "81.1496"
+//     }
+//   },
+//   "phone": "1-770-736-8031 x56442",
+//   "website": "hildegard.org",
+//   "company": {
+//     "name": "Romaguera-Crona",
+//     "catchPhrase": "Multi-layered client-server neural-net",
+//     "bs": "harness real-time e-markets"
+//   }
+// }
+
+// const getAddressString = ({ street, suite, city, zipcode }) => [city, street, suite, zipcode].join(', ');
+
+
+// users.forEach(user => {
+//  const addressString = getAddressString(user.address)
+//   const newDiv = document.createElement("div");
+
+  
+//   const content = '<span>' + '<b>ID: </b>' + user.id + '</span>' + 
+//          '<span>' + '<b>name: </b>' + user.name + '</span>' + 
+//                   '<span>' + '<b>emal: </b>'+ user.email + '</span>'
+
+//  newDiv.innerHTML =content
+
+//   document.body.appendChild(newDiv)
+
+// });
+
+
+
+function renderUsers(data) {
+
+  const ul = document.createElement('ul');
+  ul.classList.add('cards');
+
+  data.forEach(user => {
+    
+    const li = document.createElement('li');
+    li.classList.add('cards-item');
+    ul.append(li);
+    const div = document.createElement('div');
+    div.classList.add('card');
+    const id = document.createElement('p');
+    id.classList.add('card__id');
+    const name = document.createElement('p');
+    name.classList.add('card__name');
+    const userName = document.createElement('p');
+    userName.classList.add('card__username');
+    const email = document.createElement('p');
+    email.classList.add('card__email');
+    const address = document.createElement('h3');
+    address.classList.add('card__address');
+    const ulAddressList = document.createElement('ul');
+    const ulGeoList = document.createElement('ul');
+
+    const addressItems = Object.keys(user.address).map((key) => {
+      const li = document.createElement('li');
+      li.innerHTML = '<b>' + key + '</b>' + ': ' + user.address[key];
+
+      // if(user.address[key] === 'geo') {
+      //   const geoItems = Object.keys(address.geo).map((key) => {
+      //     const li = document.createElement('li');
+      //     li.innerHTML = '<b>' + key + '</b>' + ': ' + address.geo[key];
+      //     return li;
+      //   })
+      //   geoItems.forEach(node => ulGeoList.append(node));
+      // }
+
+      return li
+    })
+
+    addressItems.forEach(node => ulAddressList.append(node))
+
+
+    const street = document.createElement('p');
+    const suite = document.createElement('p');
+    const city = document.createElement('p');
+    const phone = document.createElement('a');
+    phone.classList.add('card__phone');
+    const website = document.createElement('a');
+    website.classList.add('card__website');
+    const company = document.createElement('p');
+    company.classList.add('card__company');
+    const ulCompany = document.createElement('ul');
+
+    const companyItems = Object.keys(user.company).map((key) => {
+      const li = document.createElement('li');
+      li.innerHTML = '<b>' + key + '</b>' + ': ' + user.company[key]
+
+      return li
+    })
+
+    companyItems.forEach(node => ulCompany.append(node))
+
+
+    const catchPhrase = document.createElement('p');
+    const companyName = document.createElement('p');
+    const bs = document.createElement('p');
+
+    
+    
+
+    li.append(div);
+    div.append(id, name, userName, email, address, ulAddressList, phone, website, company, ulCompany);
+    id.innerHTML = '<b>' + 'ID: ' + '</b>' + user.id;
+    name.innerHTML = '<b>' + 'Name: ' + '</b>' + '<h2>' + user.name + '</h2>';
+    userName.innerHTML = '<b>' + 'User Name: ' + '</b>' + '<h3>' + user.username + '</h3>';
+    email.innerHTML = '<b>' + 'Email: ' + '</b>' + user.email;
+    address.innerHTML = 'Address: ';
+    phone.innerHTML = '<b>' + 'Phone: ' + '</b>' + user.phone;
+    website.innerHTML = '<b>' + 'Website: ' + '</b>' + user.website;
+    company.innerHTML = '<b>' + 'Company: ' + '</b>';
+
+  });
+
+  return ul;
+}
+
+document.querySelector('.container').append(renderUsers(users));
+
+// renderUsers(users);
+
+
+// console.log(users);
+
+// var RIVERS = [{
+//   Название: "Днепр",
+//   впадает: "Черное море",
+//   длина: 2201
+// },
+// {
+//   Название: "Днестр",
+//   впадает: "Черное море",
+//   длина: 1362
+// },
+// {
+//   Название: "Южный Буг",
+//   впадает: "Черное море",
+//   длина: 806
+// },
+// {
+//   Название: "Северский Донец",
+//   впадает: "Дон",
+//   длина: 1053
+// },
+// {
+//   Название: "Горынь",
+//   впадает: "Припять",
+//   длина: 659
+// }
+// ];
+
+// function buildTable(data) {
+// const table = document.createElement('table');
+// const header = document.createElement('thead');
+// const body = document.createElement('tbody')
+
+
+// RIVERS.forEach((river, i) => {
+//   if (i === 0) {
+//     const headerRow = document.createElement('tr');
+//     Object.keys(river).map((k) => {
+//       const headerCell = document.createElement('td')
+//       headerCell.innerHTML = k;
+//       console.log(headerCell);
+//       headerRow.appendChild(headerCell);
+//       header.appendChild(headerRow);
+//     })
+//     table.appendChild(header);
+//   }
+//   const tr = document.createElement('tr')
+//   Object.values(river).map((k) => {
+//     const td = document.createElement('td')
+//     td.innerHTML = k;
+//     tr.appendChild(td);
+//   });
+//   table.appendChild(tr);
+// });
+// return table;
+// }
+// document.body.appendChild(buildTable(RIVERS));
